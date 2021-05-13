94f9f5c399881d0e08948869b76741058566f93e
marcelo <marcelomarkscarmo@gmail.com>
2021-05-12 21:31:33 -0300

    eu apaguei um codigo do qual nao estavacorreto

--- src/modules/risk-profiler/business.spec.ts
+++ src/modules/risk-profiler/business.spec.ts
@@ -1,7 +1,6 @@
 import { business } from './business';
 
 interface IPayload {
-  age: number;
   dependents: number;
   income: number;
   marital_status: 'single' | 'married';
@@ -18,55 +17,55 @@ interface IPayload {
 
 describe('riskProfiler', () => {
   let payload: IPayload;
-  beforeEach(() => {
-    payload = {
-      age: 31,
-      dependents: 0,
-      income: 220000.0,
-      marital_status: 'married',
-      risk_questions: [1, 0, 1],
-      house: {
-        ownership_status: 'mortgaged',
-      },
-      vehicle: {
-        year: 2021,
-      },
-    };
+  gta
+  payload = {
+    age: 31,
+    dependents: 0,
+    income: 220000.0,
+    marital_status: 'married',
+    risk_questions: [1, 0, 1],
+    house: {
+      ownership_status: 'mortgaged',
+    },
+    vehicle: {
+      year: 2021,
+    },
+  };
+});
+
+it('Should return a valid score from user', async () => {
+  const riskProfileResponse = await business.riskProfiler(payload);
+
+  expect(riskProfileResponse).toStrictEqual({
+    auto: 'regular',
+    disability: 'regular',
+    home: 'regular',
+    life: 'regular',
   });
+});
 
-  it('Should return a valid score from user', async () => {
-    const riskProfileResponse = await business.riskProfiler(payload);
+it('Should return a valid score from user', async () => {
+  payload.income = 0;
+  const riskProfileResponse = await business.riskProfiler(payload);
 
-    expect(riskProfileResponse).toStrictEqual({
-      auto: 'regular',
-      disability: 'regular',
-      home: 'regular',
-      life: 'regular',
-    });
+  expect(riskProfileResponse).toStrictEqual({
+    auto: 'ineligible',
+    disability: 'ineligible',
+    home: 'ineligible',
+    life: 'regular',
   });
+});
 
-  it('Should return a valid score from user', async () => {
-    payload.income = 0;
-    const riskProfileResponse = await business.riskProfiler(payload);
+it('Should return a valid score from user', async () => {
+  payload.age = 69;
+  payload.house.ownership_status = 'mortgaged';
+  const riskProfileResponse = await business.riskProfiler(payload);
 
-    expect(riskProfileResponse).toStrictEqual({
-      auto: 'ineligible',
-      disability: 'ineligible',
-      home: 'ineligible',
-      life: 'regular',
-    });
+  expect(riskProfileResponse).toStrictEqual({
+    auto: 'regular',
+    disability: 'regular',
+    home: 'responsible',
+    life: 'responsible',
   });
-
-  it('Should return a valid score from user', async () => {
-    payload.age = 69;
-    payload.house.ownership_status = 'mortgaged';
-    const riskProfileResponse = await business.riskProfiler(payload);
-
-    expect(riskProfileResponse).toStrictEqual({
-      auto: 'regular',
-      disability: 'regular',
-      home: 'responsible',
-      life: 'responsible',
-    });
-  });
+});
 });
